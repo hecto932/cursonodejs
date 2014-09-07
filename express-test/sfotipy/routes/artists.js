16c7f7a8d91308b64224e755d1f7abf1659b6a6a
Hector Flores <hecto932@gmail.com>
2014-09-07 12:33:45 -0430

    Borrando node_modules

--- express-test/sfotipy/routes/artists.js
+++ express-test/sfotipy/routes/artists.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Artist = require('../lib/artist');
+var debug = require('debug')('my-application:artists');
+var _ = require('lodash');
 
 /* GET list artists */
 router.get('/', function(req, res) {
@@ -9,9 +11,43 @@ router.get('/', function(req, res) {
   		return res.send(err);
   	}
   	res.render('artists/index', {artists:artists});
+  	//res.send({artists:artists});
   });
   
 
+});
+
+//AGREGAR UN ARTISTA A LA COLECCION
+router.get('/add', function(req, res){
+	res.render('artists/add');
+});
+
+router.get('/:artist_id', function(req, res){
+
+	Artist.findById(req.params.artist_id,function(err, artist){
+		res.render('artists/detail', {artist:artist});
+	});
+
+});
+
+router.post('/', function(req, res){
+	debug('creating new artist');
+	var name = req.body.name;
+
+	if(!_.isUndefined(name) || name !== '')
+	{
+		var artist = new Artist({name:name});
+		artist.save(function(err, artist){
+			if(err){
+				return res.send(err);
+			}
+			res.send(artist.name + 'was saved');
+		});
+	}
+	else
+	{
+		res.send('Please specify artist name');
+	}
 });
 
 module.exports = router;
--- express-test/sfotipy/scripts/db-seed.js
+++ express-test/sfotipy/scripts/db-seed.js
@@ -1,13 +1,24 @@
-var Artist = require('./lib/artist');
+var Artist = require('../lib/artist');
 
 var justin = new Artist({
 	name: 'Justin Bieber',
 	slug: 'justin-bieber',
 	albums: [
-		{title: 'Believe Acoustic', year: 2013, image 'believe-acoustic' },
-		{title: 'Belive', year: 2012, image 'believe' },
-		{title: 'Believe Acoustic', year: 2013, image 'believe-acoustic' },
-		{title: 'Believe Acoustic', year: 2013, image 'believe-acoustic' },
+		{title: 'Believe Acoustic', year: 2013, image: 'believe-acoustic' },
+		{title: 'Belive', year: 2012, image: 'believe' },
+		{title: 'Under The Mistletoe', year: 2011, image: 'under-the-mistletoe' },
+		{title: 'My World 2.0', year: 2010, image: 'my-world-2.0' }
 	],
+	shows: [
+		{city: 'Bogota'},
+		{city: 'Medellin'},
+		{city: 'Melgar'},
+		{city: 'Guadalajara'},
+		{city: 'Buenos Aires'},
+		{city: 'Brasilia'}
+	]
+});
 
+justin.save(function (err, justin){
+	console.log('Justin was saved.');
 });
